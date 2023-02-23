fa278ba425d1ee4ff2d7298e4cf2fad1862b16ee
luyosamson <luyosamson@gmail.com>
2023-02-23 18:27:58 +0300

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :productions
+  resources :productions, only: [:index, :show,:create,:update,:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get '/production', to:'productions#index'
+  # get '/productions', to:'productions#index'
+  # get '/productions/:id', to:'productions#show'
 end
