8bc89d72b3ec798dd29a258e8aa6ec1a4bcad61f
luyosamson <luyosamson@gmail.com>
2023-02-23 18:27:24 +0300

    Add controller function

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,6 +1,11 @@
 class ProductionsController < ApplicationController
 
-    def
+    def index
        render json: Production.all, status: :ok
     end
-end
+
+    def show
+        render json: Production.find(params[:id]),except:[:created_at,:updated_at] ,status: :ok
+    end
+end  
+  
