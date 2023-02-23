8d046ec81604b7cf3347324478a50d41ced06f5b
luyosamson <luyosamson@gmail.com>
2023-02-23 20:44:53 +0300

    Add controllers

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -7,5 +7,32 @@ class ProductionsController < ApplicationController
     def show
         render json: Production.find(params[:id]),except:[:created_at,:updated_at] ,status: :ok
     end
+
+    def create
+        production=Production.create(production_params)
+        render json:production, status: :created
+
+    end
+
+    def update
+        production=Production.find(params[:id])
+        production.update(production_params)
+        render json: production, status: :accepted
+    end
+
+    def destroy
+        production=Production.find(param[:id])
+        production.destroy
+        head:no_content
+    end
+
+    private
+
+    def production_params
+        params.permit(:title,:genre,:description,:image,
+        :ongoing,:budget,:director)
+
+    end
 end  
   
+
